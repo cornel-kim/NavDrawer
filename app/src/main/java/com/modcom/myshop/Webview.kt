2b35e2e429bc67665ecabc2b0e6999807bd787c2
cornel-kim <cornelkemboi@yahoo.com>
2020-10-21 07:55:53 +0300

    first commit

---
+++ app/src/main/java/com/modcom/myshop/Webview.kt
@@ -0,0 +1,17 @@
+package com.modcom.myshop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import kotlinx.android.synthetic.main.activity_webview.*
+
+
+class Webview : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_webview)
+
+        loadweb.loadUrl("file:///android_asset/contact.html")
+    }
+}
---
+++ app/src/main/java/com/modcom/myshop/ui/home/HomeFragment.kt
@@ -0,0 +1,39 @@
+package com.modcom.myshop.ui.home
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.modcom.myshop.R
+import com.modcom.myshop.Webview
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val i: Button = root.findViewById(R.id.btnweb)
+        i.setOnClickListener {
+            val i = Intent(activity, Webview::class.java)
+            startActivity(i)
+        }
+//        val textView: TextView = root.findViewById(R.id.text_home)
+//        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
+        return root
+    }
+}
